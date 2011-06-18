82f9bd6af86a9aa067b0cca276074be9e28cb82b
Matt Wildig <matt@mattwildig.co.uk>
2011-06-18 01:13:44 +0100

    Use v8 for command line testing

--- boot.js
+++ boot.js
@@ -1,9 +1,10 @@
+// little standalone file to test
+// run with v8
+
 load("utils.js");
 load("markov.js");
 
-var f = new File("alice.txt");
-f.open("read", "text");
-var txt = f.readAll().join(" ");
+var txt = read("alice.txt")
 
 var markov = new Markov(txt);
 
