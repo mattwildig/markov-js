0e8ff21818106243adc4b901c39beecebc009d5a
Matt Wildig <matt@mattwildig.co.uk>
2011-06-19 05:54:16 +0100

    Removed prefix_key function.
    Just use the arrays as keys, no need to create a string.

--- markov.js
+++ markov.js
@@ -6,30 +6,25 @@ function Markov(source) {
     var prefix_tab = {};
     
     var current_prefix = [MARKOV_SENTINEL_WORD, MARKOV_SENTINEL_WORD];
-    
-    function prefixKey() {return current_prefix.join("|");}
-    
+        
     for (var i = 0; i<words.length; i++) {
-        
-        var key = prefixKey();
-        
-        prefix_tab[key] = prefix_tab[key] || [];
-        prefix_tab[key].push(words[i]);
+                
+        prefix_tab[current_prefix] = prefix_tab[current_prefix] || [];
+        prefix_tab[current_prefix].push(words[i]);
         
         current_prefix.shift();
         current_prefix.push(words[i]);
     }
     
-    prefix_tab[prefixKey()] = prefix_tab[prefixKey()] || [];        
-    prefix_tab[prefixKey()].push(MARKOV_SENTINEL_WORD);
+    prefix_tab[current_prefix] = prefix_tab[current_prefix] || [];        
+    prefix_tab[current_prefix].push(MARKOV_SENTINEL_WORD);
     
     this.generate = function(maxwords) {
         var generated = "";
         var current_prefix = [MARKOV_SENTINEL_WORD, MARKOV_SENTINEL_WORD];
         
         while(maxwords-- > 0) {
-            var key = current_prefix.join("|");
-            var list = prefix_tab[key];  
+            var list = prefix_tab[current_prefix];  
             var nextWord = list[Math.floor(Math.random() * list.length)];
             
             if(nextWord == MARKOV_SENTINEL_WORD) break;
